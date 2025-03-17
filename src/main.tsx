ea62380c95376b0efbe74e57d20b3b28a519510d
Tal Ben Adon <talbenadon2@gmail.com>
2025-03-17 17:01:24 +0900

    first commit, set up simple react-router with pages

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import { StrictMode } from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createRoot } from "react-dom/client"
+import Root from "./pages/Root"
+import Home from "./pages/Home"
+import SessionsPage from "./pages/Sessions"
+import SessionPage from "./pages/Session"
+import './index.css'
+
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      { index: true, Component: Home, },
+      { path: "sessions", Component: SessionsPage },
+      { path: "sessions:id", Component: SessionPage }
+    ]
+  }
+])
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
