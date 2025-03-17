789e5d5a4ffb44946a368bec2532417f85191c2b
Tal Ben Adon <talbenadon2@gmail.com>
2025-03-17 22:00:50 +0900

    main header navigation skeleton

--- src/Components/Navigation/MainHeader.tsx
+++ src/Components/Navigation/MainHeader.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
+
 export default function MainHeader() {
+    const [upcomingSessionsVisible, setUpcomingSessionsVisible] = useState(false)
+
     return (
-        <div>
-
-
-        </div>
+        <header>
+            <h1></h1>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to={"/"} end>
+                            Our Mission
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={"sessions"}>
+                            Browse Sessions
+                        </NavLink>
+                    </li>
+                    <li>
+                        <button onClick={() => { }}>Upcoming Sessions</button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
