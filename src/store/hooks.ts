4dc9e88cf7c77527383532d21361dde7c07c6860
Tal Ben Adon <talbenadon2@gmail.com>
2025-04-10 02:29:16 +0900

    store and pages comments added, modal comments adjusted

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,7 +1,13 @@
 import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux";
 import { AppDispatch, RootState } from "./store";
 
+// ***custom redux dispatch and selector hooks *** // 
+
+// Creating a custom type which is a function that returns the AppDispatch (type of store.dispatch)
 type DispatchFunction = () => AppDispatch
 
-export const useBookedSessionsDispatch: DispatchFunction = useDispatch; // no need to recreate those hooks for each slice. can be called useAppDispatch etc.
+// Attaches the hook to access redux dispatch function to our own typed function. No need to recreate those hooks for each slice. can be called useAppDispatch etc.
+export const useBookedSessionsDispatch: DispatchFunction = useDispatch;
+
+// Attaches the hook to access ridux store state to our own typed function. We continue to attach it with our RootState type
 export const useBookedSessionsSelector: TypedUseSelectorHook<RootState> = useSelector
