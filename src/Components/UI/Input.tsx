7b69420e2fce663cd9b91ae55a063325f118e44e
Tal Benadon <talbenadon2@gmail.com>
2025-04-10 10:23:57 +0900

    input.tsx comment

--- src/Components/UI/Input.tsx
+++ src/Components/UI/Input.tsx
@@ -1,5 +1,9 @@
 import { ComponentPropsWithoutRef } from "react";
 
+// *** custom input component *** // 
+
+
+// Input props type tells us it needs a label and id, and anything in between something an input tag can receive.
 type InputProps = {
     label: string;
     id: string;
